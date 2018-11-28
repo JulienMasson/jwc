cbc99b05e5730ca66d75b5c46f7fa72202cc9462
Julien Masson <massonju.eseo@gmail.com>
2018-11-28 13:05:55 +0100

    utils: add macro for wlr_log functions
    Signed-off-by: Julien Masson <massonju.eseo@gmail.com>

---
+++ src/utils.h
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the jwm distribution:
+ * https://github.com/JulienMasson/jwc
+ *
+ * Copyright (c) 2018 Julien Masson.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "server.h"
+
+#define ERROR(...)	wlr_log(WLR_ERROR, __VA_ARGS__)
+#define INFO(...)	wlr_log(WLR_INFO, __VA_ARGS__)
+#define DEBUG(...)	wlr_log(WLR_DEBUG, __VA_ARGS__)
+
+#endif
