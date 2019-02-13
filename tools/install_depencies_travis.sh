be9518e94e7a788c6f07c6dc87fbc7c3eb36bd47
Julien Masson <massonju.eseo@gmail.com>
2019-02-13 10:42:42 +0100

    travis: install manually libxcb/xcb-proto version 1.13.1
    Signed-off-by: Julien Masson <massonju.eseo@gmail.com>

--- tools/install_depencies_travis.sh
+++ tools/install_depencies_travis.sh
@@ -8,7 +8,7 @@ mkdir deps && cd deps
 
 # system packages
 sudo apt-get update -qq
-sudo apt-get install git python3 python3-pip ninja-build doxygen xmlto graphviz libudev-dev libmtdev-dev libevdev-dev libwacom-dev libgtk2.0-dev libgtk-3-dev libegl1-mesa-dev libgles2-mesa-dev libpixman-1-dev libxkbcommon-dev libgbm-dev libmount-dev gobject-introspection libgirepository1.0-dev libfribidi-dev libthai-dev gtk-doc-tools xutils-dev libglib2.0-dev libcairo2-dev libgdk-pixbuf2.0-dev libharfbuzz-dev libpango1.0-dev libepoxy-dev check valgrind libxcb1-dev libxcb-composite0-dev libxcb-xfixes0-dev libxcb-image0-dev libxcb-render0-dev libx11-xcb-dev libxcb-icccm4-dev libxcb-xkb-dev libpciaccess-dev -y
+sudo apt-get install git python3 python3-pip ninja-build doxygen xmlto graphviz libudev-dev libmtdev-dev libevdev-dev libwacom-dev libgtk2.0-dev libgtk-3-dev libegl1-mesa-dev libgles2-mesa-dev libpixman-1-dev libxkbcommon-dev libgbm-dev libmount-dev gobject-introspection libgirepository1.0-dev libfribidi-dev libthai-dev gtk-doc-tools xutils-dev libglib2.0-dev libcairo2-dev libgdk-pixbuf2.0-dev libharfbuzz-dev libpango1.0-dev libepoxy-dev check valgrind libxcb1-dev libxcb-composite0-dev libxcb-xfixes0-dev libxcb-image0-dev libxcb-render0-dev libx11-xcb-dev libxcb-icccm4-dev libxcb-xkb-dev libpciaccess-dev python-xcbgen -y
 sudo pip3 install --upgrade pip
 sudo pip3 install setuptools sphinx
 sudo pip3 install recommonmark
@@ -31,6 +31,20 @@ git clone https://github.com/mesonbuild/meson.git
 pushd meson
 git checkout 0.48.1
 sudo python3 setup.py install
+popd
+
+# install xcb-proto version 1.13.1
+git clone https://anongit.freedesktop.org/git/xcb/proto.git
+pushd proto
+git checkout xcb-proto-1.13
+autotools_install
+popd
+
+# install libxcb version 1.13.1
+git clone https://anongit.freedesktop.org/git/xcb/libxcb.git
+pushd libxcb
+git checkout 1.13.1
+autotools_install --without-doxygen
 popd
 
 # install wayland
@@ -63,12 +77,6 @@ git clone https://gitlab.freedesktop.org/mesa/drm.git libdrm
 pushd libdrm
 meson_install
 popd
-
-# install xcb-xinput
-wget http://ftp.fr.debian.org/debian/pool/main/libx/libxcb/libxcb-xinput0_1.13.1-2_amd64.deb
-sudo dpkg -i libxcb-xinput0_1.13.1-2_amd64.deb
-wget http://ftp.fr.debian.org/debian/pool/main/libx/libxcb/libxcb-xinput-dev_1.13.1-2_amd64.deb
-sudo dpkg -i libxcb-xinput-dev_1.13.1-2_amd64.deb
 
 # libxkbcommon version 0.8.2
 git clone https://github.com/xkbcommon/libxkbcommon.git
